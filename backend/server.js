e2e998d0405dd8bc5a2bfa3be2b84f7ee731f0ca
Veetrag Choudhary <131524761+VeetragChoudhary@users.noreply.github.com>
2024-07-28 13:26:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 
-
+app.use(cors())
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
@@ -18,7 +18,7 @@ const __dirname = path.resolve()
 
 dotenv.config()
 
-app.use(cors())
+
 app.use(express.json())
 app.use(cookieParser())
 
