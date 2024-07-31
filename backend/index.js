989ecc97c3ed290f8500d737bf337a938ecd36e5
Veetrag Choudhary <131524761+VeetragChoudhary@users.noreply.github.com>
2024-07-31 10:41:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,12 +20,10 @@ const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
 
 dotenv.config()
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+app.options('*', (req, res) => {
+    res.sendStatus(200);
 });
+
 
 app.use(cors());
 app.use(express.json())
