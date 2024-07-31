a430926800d3aa97ffebf8535f90cf28cffb21ca
Veetrag Choudhary <131524761+VeetragChoudhary@users.noreply.github.com>
2024-07-31 10:56:36 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -13,7 +13,7 @@ const useLogin = () => {
         try {
             const res = await fetch("https://chat-app-jade-mu.vercel.app/api/auth/login", {
                 method: "POST",
-                headers: { "Content-Type": "application/json" },
+                
                 body: JSON.stringify({ username, password }),
             })
 
